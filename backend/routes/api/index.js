562a1bf4aca7270087f43ebb5e0d25e162c9f0e2
Ran Wang <wangyufei0410@gmail.com>
2023-11-02 04:06:35 -0700

    delete-a-spot-image

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewRouter = require('./reviews.js');
 const bookingRouter = require('./bookings.js');
+const spotImageRouter = require('./spot-images.js');
 const { restoreUser } = require("../../utils/auth.js");
 const {requireAuth} = require('../../utils/auth');
 router.get('/test',requireAuth,(req,res)=>{
@@ -20,6 +21,7 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews',reviewRouter);
 router.use('/bookings', bookingRouter);
+router.use('/spot-images', spotImageRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
