9c0e20d234c34bccb0c6ac7e1eb5eefa2cbd8f5a
Ran Wang <wangyufei0410@gmail.com>
2023-11-17 13:46:12 -0800

    bonus phase-Nest Login And Signup Buttons In Dropdown Menu1

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,45 +1,23 @@
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import OpenModalButton from '../OpenModalButton/OpenModalButton';
-import LoginFormModal from '../LoginFormModal/LoginFormModal';
-import SignupFormModal from '../SignupFormModal/SignupFormModal';
-import './Navigation.css';
+// import { NavLink } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import ProfileButton from './ProfileButton';
+// import OpenModalButton from '../OpenModalButton/OpenModalButton';
+// import LoginFormModal from '../LoginFormModal/LoginFormModal';
+// import SignupFormModal from '../SignupFormModal/SignupFormModal';
+// import './Navigation.css';
 
-function Navigation({ isLoaded }) {
-  const sessionUser = useSelector((state) => state.session.user);
-  let sessionLinks;
-  if (sessionUser) {
-    sessionLinks = (
-      <li>
-        <ProfileButton user={sessionUser} />
-      </li>
-    );
-  } else {
-    sessionLinks = (
-        <>
-      <li>
-        <OpenModalButton
-          buttonText="Log In"
-          modalComponent={<LoginFormModal />}
-        />
-      </li>
-      <li>
-        <OpenModalButton
-          buttonText="Sign Up"
-          modalComponent={<SignupFormModal />}
-        />
-      </li>
-      </>
-    );
-  }
-
-//   const sessionLinks = sessionUser ? (
-//     <li>
-//       <ProfileButton user={sessionUser} />
-//     </li>
-//   ) : (
-//     <>
+// function Navigation({ isLoaded }) {
+//   const sessionUser = useSelector((state) => state.session.user);
+//   let sessionLinks;
+//   if (sessionUser) {
+//     sessionLinks = (
+//       <li>
+//         <ProfileButton user={sessionUser} />
+//       </li>
+//     );
+//   } else {
+//     sessionLinks = (
+//         <>
 //       <li>
 //         <OpenModalButton
 //           buttonText="Log In"
@@ -47,18 +25,45 @@ function Navigation({ isLoaded }) {
 //         />
 //       </li>
 //       <li>
-//         <NavLink to="/signup">Sign Up</NavLink>
+//         <OpenModalButton
+//           buttonText="Sign Up"
+//           modalComponent={<SignupFormModal />}
+//         />
 //       </li>
-//     </>
-//   );
+//       </>
+//     );
+//   }
 
+//   return (
+//     <ul>
+//       <li>
+//         <NavLink to="/">Home</NavLink>
+//       </li>
+//       {isLoaded && sessionLinks}
+//     </ul>
+//   );
+// }
+
+// export default Navigation;
+
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
+import './Navigation.css';
+
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector(state => state.session.user);
 
   return (
     <ul>
       <li>
-        <NavLink to="/">Home</NavLink>
+        <NavLink exact to="/">Home</NavLink>
       </li>
-      {isLoaded && sessionLinks}
+      {isLoaded && (
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
     </ul>
   );
 }
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,6 +1,64 @@
+// import { useState, useEffect, useRef } from 'react';
+// import { useDispatch } from 'react-redux';
+// import * as sessionActions from '../../store/session';
+
+// function ProfileButton({ user }) {
+//   const dispatch = useDispatch();
+//   const [showMenu, setShowMenu] = useState(false);
+//   const ulRef = useRef();
+
+//   const toggleMenu = (e) => {
+//     e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
+//     // if (!showMenu) setShowMenu(true);
+//     setShowMenu(!showMenu);
+//   };
+
+//   useEffect(() => {
+//     if (!showMenu) return;
+
+//     const closeMenu = (e) => {
+//       if (!ulRef.current.contains(e.target)) {
+//         setShowMenu(false);
+//       }
+//     };
+
+//     document.addEventListener('click', closeMenu);
+
+//     return () => document.removeEventListener("click", closeMenu);
+//   }, [showMenu]);
+
+//   const logout = (e) => {
+//     e.preventDefault();
+//     dispatch(sessionActions.logout());
+//   };
+
+//   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
+//   return (
+//     <>
+//       <button onClick={toggleMenu}>
+//         <i className="fas fa-user-circle" />
+//       </button>
+//       <ul className={ulClassName} ref={ulRef}>
+//         <li>{user.username}</li>
+//         <li>{user.firstName} {user.lastName}</li>
+//         <li>{user.email}</li>
+//         <li>
+//           <button onClick={logout}>Log Out</button>
+//         </li>
+//       </ul>
+//     </>
+//   );
+// }
+
+// export default ProfileButton;
+
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import OpenModalButton from '../OpenModalButton/OpenModalButton';
+import LoginFormModal from '../LoginFormModal/LoginFormModal';
+import SignupFormModal from '../SignupFormModal/SignupFormModal';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
@@ -8,8 +66,7 @@ function ProfileButton({ user }) {
   const ulRef = useRef();
 
   const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
-    // if (!showMenu) setShowMenu(true);
+    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
     setShowMenu(!showMenu);
   };
 
@@ -40,12 +97,31 @@ function ProfileButton({ user }) {
         <i className="fas fa-user-circle" />
       </button>
       <ul className={ulClassName} ref={ulRef}>
-        <li>{user.username}</li>
-        <li>{user.firstName} {user.lastName}</li>
-        <li>{user.email}</li>
-        <li>
-          <button onClick={logout}>Log Out</button>
-        </li>
+        {user ? (
+          <>
+            <li>{user.username}</li>
+            <li>{user.firstName} {user.lastName}</li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <OpenModalButton
+                buttonText="Log In"
+                modalComponent={<LoginFormModal />}
+              />
+            </li>
+            <li>
+              <OpenModalButton
+                buttonText="Sign Up"
+                modalComponent={<SignupFormModal />}
+              />
+            </li>
+          </>
+        )}
       </ul>
     </>
   );
