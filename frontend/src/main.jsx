44b45acd1be59bc6caee0f916c8a1685fcb990b6
Ran Wang <wangyufei0410@gmail.com>
2023-11-17 00:31:14 -0800

    phase1-log in1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,16 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store/store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
+
 if (import.meta.env.MODE !== 'production') {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 if (process.env.NODE_ENV !== 'production') {
