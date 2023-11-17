d481b414a551c35c31e799bd4dd5e3fde1a828d3
Ran Wang <wangyufei0410@gmail.com>
2023-11-17 00:53:31 -0800

    phase1-login3-Restore the session user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,47 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage/LoginFormPage';
+import * as sessionActions from './store/session';
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
 
 const router = createBrowserRouter([
+  // {
+  //   path: '/',
+  //   element: <h1>Welcome!</h1>
+  // },
+  // {
+  //   path: '/login',
+  //   element: <LoginFormPage />
+  // }
   {
-    path: '/',
-    element: <h1>Welcome!</h1>
-  },
-  {
-    path: '/login',
-    element: <LoginFormPage />
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      },
+      {
+        path: '/login',
+        element: <LoginFormPage />
+      }
+    ]
   }
 ]);
 
@@ -17,8 +50,3 @@ function App() {
 }
 
 export default App;
-// function App() {
-//   return <h1> Hello from App </h1>;
-// }
-
-// export default App;
