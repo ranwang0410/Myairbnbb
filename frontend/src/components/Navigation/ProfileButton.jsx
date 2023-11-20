fcd9aa2761ba0f825ded15a182ef07c771477e71
Ran Wang <wangyufei0410@gmail.com>
2023-11-20 11:06:59 -0800

    modify header css

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,57 +1,3 @@
-// import { useState, useEffect, useRef } from 'react';
-// import { useDispatch } from 'react-redux';
-// import * as sessionActions from '../../store/session';
-
-// function ProfileButton({ user }) {
-//   const dispatch = useDispatch();
-//   const [showMenu, setShowMenu] = useState(false);
-//   const ulRef = useRef();
-
-//   const toggleMenu = (e) => {
-//     e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
-//     // if (!showMenu) setShowMenu(true);
-//     setShowMenu(!showMenu);
-//   };
-
-//   useEffect(() => {
-//     if (!showMenu) return;
-
-//     const closeMenu = (e) => {
-//       if (!ulRef.current.contains(e.target)) {
-//         setShowMenu(false);
-//       }
-//     };
-
-//     document.addEventListener('click', closeMenu);
-
-//     return () => document.removeEventListener("click", closeMenu);
-//   }, [showMenu]);
-
-//   const logout = (e) => {
-//     e.preventDefault();
-//     dispatch(sessionActions.logout());
-//   };
-
-//   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
-
-//   return (
-//     <>
-//       <button onClick={toggleMenu}>
-//         <i className="fas fa-user-circle" />
-//       </button>
-//       <ul className={ulClassName} ref={ulRef}>
-//         <li>{user.username}</li>
-//         <li>{user.firstName} {user.lastName}</li>
-//         <li>{user.email}</li>
-//         <li>
-//           <button onClick={logout}>Log Out</button>
-//         </li>
-//       </ul>
-//     </>
-//   );
-// }
-
-// export default ProfileButton;
 
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
@@ -60,14 +6,14 @@ import OpenModalMenuItem from './OpenModalMenuItem';
 // import OpenModalButton from '../OpenModalButton/OpenModalButton';
 import LoginFormModal from '../LoginFormModal/LoginFormModal';
 import SignupFormModal from '../SignupFormModal/SignupFormModal';
-
+import './ProfileButton.css'
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
 
   const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+    e.stopPropagation();
     setShowMenu(!showMenu);
   };
 
