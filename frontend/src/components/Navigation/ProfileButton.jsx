93eafd02c681b26fb23cc0f4e0b77e9d3ae67a27
Ran Wang <wangyufei0410@gmail.com>
2023-11-28 08:04:44 -0800

    log1

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -6,8 +6,7 @@ import OpenModalMenuItem from './OpenModalMenuItem';
 // import OpenModalButton from '../OpenModalButton/OpenModalButton';
 import LoginFormModal from '../LoginFormModal/LoginFormModal';
 import SignupFormModal from '../SignupFormModal/SignupFormModal';
-import './ProfileButton.css'
-import { NavLink } from 'react-router-dom'
+
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
@@ -47,7 +46,6 @@ function ProfileButton({ user }) {
       <button onClick={toggleMenu}>
         <i className="fas fa-user-circle" />
       </button>
-<<<<<<< HEAD
       {
         showMenu &&
         <ul className={ulClassName} ref={ulRef}>
@@ -57,26 +55,11 @@ function ProfileButton({ user }) {
               <li>{user.firstName} {user.lastName}</li>
               <li>{user.email}</li>
               <li>
-=======
-
-        <ul className={ulClassName} ref={ulRef}>
-          {user ? (
-            <>
-
-              <li>Hello, {user.username}</li>
-              {/* <li>{user.firstName} {user.lastName}</li> */}
-              <li>{user.email}</li>
-              <li>
-              <NavLink to="/manage-spots">Manage Spots</NavLink>
-            </li>
-              <li>
->>>>>>> dev
                 <button onClick={logout}>Log Out</button>
               </li>
             </>
           ) : (
             <>
-<<<<<<< HEAD
               <li>
                 <OpenModalMenuItem
                   itemText="Log In"
@@ -85,35 +68,16 @@ function ProfileButton({ user }) {
                 />
               </li>
               <li>
-=======
-            <span>
->>>>>>> dev
                 <OpenModalMenuItem
                   itemText="Sign Up"
                   onButtonClick={closeMenu}
                   modalComponent={<SignupFormModal />}
                 />
-<<<<<<< HEAD
               </li>
             </>
           )}
         </ul>
       }
-=======
-              </span>
-              <span>
-                <OpenModalMenuItem
-                  itemText="Log In"
-                  onButtonClick={closeMenu}
-                  modalComponent={<LoginFormModal />}
-                />
-              </span>
-
-            </>
-          )}
-        </ul>
-
->>>>>>> dev
     </>
   );
 }
